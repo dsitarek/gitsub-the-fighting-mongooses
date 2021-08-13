a86c70bf68906d7f2e52b8404ce63beb93f58dbc
david vareba <davidvareba@gmail.com>
2021-08-12 20:11:44 -0500

    fixed data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-export const packageArray = [
+export const repoArray = [
   {
     name: "gatsby-workshop",
     description: "This workshop covers the fundamentals of developing fast, accessible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites.",
